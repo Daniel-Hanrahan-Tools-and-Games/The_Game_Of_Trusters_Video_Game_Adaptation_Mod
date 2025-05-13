b5411147e8744813c93a03006362c1c1f0422d84
Daniel-Hanrahan-Tools-and-Games <106982630+Daniel-Hanrahan-Tools-and-Games@users.noreply.github.com>
2025-05-13 17:55:14 -0400

    Add files via upload
    The mod and artwork for that mod

---
+++ The_Game_Of_Trusters_Mod.lua
@@ -0,0 +1,17 @@
+-- Notice and Name string
+strNoticeAndName = "The_Game_of_Trusters_Mod" .. "\n" .. "Copyright (C) 2025 Daniel Hanrahan Tools and Games" .. "\n" .. "This Expansion or Mod is under the BSD-3-Clause license and everything else in this Expansion/Mod is under this: This work is licensed under CC BY 4.0 \n"
+
+-- Dice integer
+intDice = math.random(1, 2);
+
+-- Enemy One string
+strEnemy1 = "You have encountered a slime, what will you do, 0:attack, 1:Talk: "
+
+-- Enemy Two string
+strEnemy2 = "You have encountered a ghost, what will you do, 0:attack, 1:Talk: "
+
+-- Player Location State 1 string
+strPlayerLocationState1 = "you are in a forest, do you want to turn left(1) or right(2): "
+
+-- Player Location State 2 string
+strPlayerLocationState2 = "you are in a desert, do you want to turn left(1) or right(2): "
